5f4171fedce49240529fed160b8cd08ecdd8bc32
Xodeen <s2410238006@students.fh-hagenberg.at>
2026-03-17 13:14:22 +0100

    Add PlayerMovement and update scene + IOTest
    Add PlayerMovement.cs (and meta) and register a Player GameObject in SampleScene; major scene changes include a Camera rename/adjustment, EventSystem, ground and multiple GameObjects (sprites/colliders/renderers) added to scene roots. Update IOTest.cs to be a singleton, add serial read handling for Arduino-style input (jump/duck), expose jumpedPressed/duckPressed flags, and shorten serial timeouts. Also update packages manifest/lock and project settings to reflect editor/URP changes.

--- Assets/Scripts/IOTest.cs
+++ Assets/Scripts/IOTest.cs
@@ -1,44 +1,83 @@
 using System;
 using UnityEngine;
 using System.IO.Ports;
+using NUnit.Framework.Constraints;
+using UnityEngine.InputSystem;
 
 public class IOTest : MonoBehaviour
 {
     private SerialPort serialPort;
+    
+    public bool jumpedPressed = false;
+    public bool duckPressed = false;
+
+    private InputAction jumpAction;
+    private PlayerMovement pm;
+
+    //Singleton
+    public static IOTest instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         serialPort = new SerialPort("COM5", 9600);
         serialPort.Open();
-        serialPort.ReadTimeout = 5000;
+        serialPort.ReadTimeout = 100;
+        serialPort.WriteTimeout = 100;
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         if (!serialPort.IsOpen) return;
-        string action = "";
-        try
-        { 
-            action = serialPort.ReadLine();
-        }
-        catch (Exception e){}
-        
 
-        switch (action)
+        try
         {
-            case "Jump Pressed":
-                Debug.Log(action);
-                break;
-            case "Jump Released":
-                Debug.Log(action);
-                break;
-            case "Duck Pressed":
-                Debug.Log(action);
-                break;
-            case "Duck Released":
-                Debug.Log(action);
-                break;
+            string action = serialPort.ReadLine();
+            switch (action)
+            {
+                case "Jump Pressed":
+                    Debug.Log(action);
+                    jumpedPressed = true;
+                    break;
+                case "Jump Released":
+                    Debug.Log(action);
+                    jumpedPressed = false;
+                    break;
+                case "Duck Pressed":
+                    Debug.Log(action);
+                    duckPressed = true;
+                    break;
+                case "Duck Released":
+                    Debug.Log(action);
+                    duckPressed = false;
+                    break;
+            }
         }
+        catch (Exception e)
+        {
+            //Ignore
+        }
+        
+        
+    }
+
+    private void OnApplicationQuit()
+    {
+        serialPort.Close();
     }
 }
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField]
+    private IOTest ioTest;
+    [SerializeField]
+    private Rigidbody2D _rb;
+    
+    [Header("Movement")]
+    [SerializeField]
+    private Vector2 velocity;
+    public bool _isGrounded = false;
+    
+    [Header("Stats")]
+    [SerializeField] private float jumpForce = 3f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Mathf.Abs(_rb.linearVelocity.y) <= 0){ _isGrounded = true;}else{ _isGrounded = false; }
+        
+        if (IOTest.instance.jumpedPressed)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        if (!_isGrounded) return;
+        _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); //Add force to y (up) direction 
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Obstacle")
+        {
+            Debug.Log("Obstacle hit: " + other.gameObject.name);
+        }
+    }
+    
+}
