5de5f4d3294454ddb48c494e184d3728b057492a
Jaci Baumgartner <S2410238005@fhooe.at>
2026-05-22 17:28:15 +0200

    Test Fail Button

--- Assets/Scenes/Jaci/Script/PipeBouncingGame.cs
+++ Assets/Scenes/Jaci/Script/PipeBouncingGame.cs
@@ -3,18 +3,20 @@ using System.Collections;
 using UnityEngine.UI;
 using System;
 using Unity.VisualScripting;
+using System.Threading.Tasks;
 
-public class PipeBouncingGame : MonoBehaviour
+public class PipeBouncingGame : BombMinigame
 {
 
     //Frage: Welches Objekt ist der Balken?
-    [SerializeField] private Transform movingBar;
+    [SerializeField] private GameObject movingBar;
+    [SerializeField] private GameObject[] movingBalls;
     //Frage: Wie schnell bewegt er sich?
     [SerializeField] private float moveSpeed = 1.5f;
     //Frage: Wie weit darf er sich bewegen?
     [SerializeField] private float minY = 0.6902707f;  
     [SerializeField] private float maxY = 1.72f;
-
+    
     [SerializeField] private float cooldownTime = 5f; // Einstellung für die Strafwartezeit vom Bouncingspiel
 
     // Frage: In welche Richtung bewegt es sich gerade?
@@ -23,31 +25,19 @@ public class PipeBouncingGame : MonoBehaviour
     private bool isMoving = true; //Kontrolle ob sich das Ding bewegt
 
     private bool isInTarget = false; //Kontrolle ob es in der Zielfläche drin ist.
+
+    private bool isFinish = false; // bedeutet das es noch nicht startet.
+
+
+    void Start()
+    {
+        StartMinigame();
+    }
     
     
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        Debug.Log("Pipe Minigame startet");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        Debug.Log("Update wird ausgeführt");
-
-        if (isMoving)
-        {
-            MoveBar();
-        }
-        
-
-
-    }
-
     public void Stop()
     {
         if (!isMoving) return; 
@@ -86,21 +76,25 @@ public class PipeBouncingGame : MonoBehaviour
         //Prüfen: Ist der Balken unten angekommen?
         //Wenn ja: Richtung umdrehen
         //Neue Position wieder speichern
-
-        Vector3 position = movingBar.position;
-
-        position.y += direction * moveSpeed * Time.deltaTime; //Time.deltaTime ist die Framrate damit es flüssig ausschaut. 
-
-        if (position.y >= maxY)  //Cube geht bis zum Max Wert
+        foreach(var ball in movingBalls)
         {
-            direction = -1;
-        }
-        if (position.y <= minY) // Cube geht bis zum Min Wert
-        {
-            direction = 1;
+                Vector3 position = ball.transform.position;
+
+                position.y += direction * moveSpeed * Time.deltaTime; //Time.deltaTime ist die Framrate damit es flüssig ausschaut. 
+
+            if (position.y >= maxY)  //Cube geht bis zum Max Wert
+                {
+                    direction = -1;
+                }
+            if (position.y <= minY) // Cube geht bis zum Min Wert
+                {
+                    direction = 1;
+                }
+
+            ball.transform.position = position;
         }
 
-        movingBar.position = position;
+  
 
     }
 
@@ -122,5 +116,47 @@ public class PipeBouncingGame : MonoBehaviour
         }
     }
 
+    private IEnumerator LoopBallBounce()
+    {
+        while (isStarted)
+        {
+            MoveBar();
 
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public override void SetEventListeners()
+    {
+        IOHandler.buttonUsed.AddListener(Stop);
+
+    }
+
+    public override void UnbindEventListeners()
+    {
+        IOHandler.buttonUsed.RemoveListener(Stop);
+    }
+
+    public override void StartMinigame()
+    {
+        isStarted = true; 
+        movingBar = movingBalls[0];
+        StartCoroutine(LoopBallBounce()); 
+    }
+
+    public override async Awaitable<bool> EnterMinigame()
+    {
+        SetEventListeners();
+        while (!isFinish)
+        {
+            await Awaitable.NextFrameAsync();
+        }
+        isFinish = false; 
+        return true;
+    }
+
+    public override void ExitMinigame()
+    {
+        UnbindEventListeners();
+    }
 }
